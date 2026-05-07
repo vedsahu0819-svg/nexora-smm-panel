185378f7f6bd6b134cec81703fa3fc2ec7461b16
vedsahu0819-svg <vedsahu0819@gmail.com>
2026-05-08 00:42:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
         <div style={{ display: "flex", gap: "24px" }}>
           <a
-            href="#"
+            href="/services"
             style={{
               color: "#cbd5e1",
               textDecoration: "none",
@@ -42,7 +42,7 @@ export default function Page() {
           </a>
 
           <a
-            href="#"
+            href="/services"
             style={{
               color: "#cbd5e1",
               textDecoration: "none",
@@ -52,7 +52,7 @@ export default function Page() {
           </a>
 
           <a
-            href="#"
+            href="/services"
             style={{
               color: "#cbd5e1",
               textDecoration: "none",
